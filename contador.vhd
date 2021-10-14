315b4d5ed6e9602b05394581b9012c9b7c15188b
TheBirb <mmjontxu@gmail.com>
2021-10-14 13:50:41 +0200

    v0.2
    Se han añadido los registros X e Y además de los estados que faltaban de carga de datos. Se han añadido nuevas señales de entrada.

--- contador.vhd
+++ contador.vhd
@@ -6,8 +6,11 @@ ENTITY counter8 IS
   PORT (
     clk       : IN std_logic; 
     reset     : IN std_logic; 
-    count     : OUT std_logic_vector(3 DOWNTO 0);
-    tc        : OUT std_logic;
+    LCD_init_done : IN std_logic;
+    OP_SETCURSOR : IN std_logic;
+    OP_DRAWCOLOR : IN std_logic;
+    YROW      : IN std_logic_vector(8 DOWNTO 0);
+    XCOL      : IN std_logic_vector(7 DOWNTO 0);
     LCD_WRN   : OUT std_logic;
     LCD_RS    : OUT std_logic;
     LCD_CSN   : OUT std_logic
@@ -15,53 +18,82 @@ ENTITY counter8 IS
 END counter8;
  
 ARCHITECTURE arch1 OF counter8 IS
-   TYPE ESTADO IS (Estado0,Estado1,Estado2, Estado3,Estado9,Estado10,Estado11);
+   TYPE ESTADO IS (Estado0,Estado1,Estado2, Estado3,Estado9,Estado10,Estado11,ProcesarC,EsperaOP,Inicio,ProcesarD);
    SIGNAL estado_q, estado_d: ESTADO;
-   SIGNAL cnt : unsigned(3 DOWNTO 0);
-   SIGNAL ENABLE_DATA: std_logic;
-   
+   SIGNAL cnt : unsigned(2 DOWNTO 0);
+   SIGNAL ENABLE_CONT_DATA: std_logic;
+   SIGNAL x : std_logic_vector(7 DOWNTO 0);
+   SIGNAL y :  std_logic_vector(8 DOWNTO 0);
+   SIGNAL LD_X: std_logic;
+   SIGNAL LD_Y: std_logic;
+   SIGNAL LD_CONT_DATA: std_logic;
+   SIGNAL LD_VALUE_CONT_DATA : unsigned(2 DOWNTO 0);
+ 	
    
 BEGIN
-   PROCESS (estado_q,cnt)
+   PROCESS (estado_q,cnt,OP_SETCURSOR, OP_DRAWCOLOR)
    begin
 	case estado_q is
+	 when Inicio => if LCD_init_done='1' then estado_d<=EsperaOP; else estado_d<=Inicio; end if; --al ponerlo a 1 en la sim no cambia de estado?
+	 when EsperaOP => if OP_SETCURSOR='1' then estado_d<=ProcesarC; elsif OP_DRAWCOLOR='1' then estado_d<=ProcesarD; else estado_d<=EsperaOP; end if;
+	 when ProcesarC => estado_d<=Estado0;
+	 when ProcesarD => estado_d<=EsperaOP; --TODO
 	 when Estado0 => estado_d<=Estado1;
 	 when Estado1 => estado_d<=Estado2;
 	 when Estado2 => estado_d<=Estado3;
 	 when Estado3 => if cnt="000" or cnt = "011" then estado_d<=Estado9; elsif cnt = "001" or cnt="010" or cnt="100" or cnt="101" then estado_d <= Estado10;else estado_d<=Estado11; end if;
 	 when Estado9 => estado_d<=Estado0;
 	 when Estado10 => estado_d<=Estado0;
-	 when others => estado_d<=Estado11;
+	 when Estado11 => estado_d<=EsperaOP;
+	 when others => estado_d<=EsperaOP;
  	end case;
    end process; 
+
 LCD_WRN <='1' when (estado_q=Estado1) or (estado_q=Estado2) or (estado_q=Estado3) else '0';
 LCD_CSN <='1' when (estado_q=Estado1) or (estado_q=Estado2) or (estado_q=Estado3) else '0';
 LCD_RS  <='1' when (estado_q=Estado10) else '0';
-ENABLE_DATA <='1' when (estado_q=Estado3) else '0';
-
+ENABLE_CONT_DATA <='1' when (estado_q=Estado3) else '0';
+LD_X <='1' when (estado_q=ProcesarC) else '0';
+LD_Y <='1' when (estado_q=ProcesarC) else '0';
+LD_VALUE_CONT_DATA <= "000" when (estado_q=ProcesarC) else "110" when (estado_q=ProcesarD) else "000";
+LD_CONT_DATA <= '1' when (estado_q=ProcesarC) or (estado_q=ProcesarD) else '0';
 --registro de estado
    PROCESS (clk,reset)
    begin
-   if reset='1' then
-	estado_q<=Estado0;
-   elsif (clk'event and clk='1') then
-	estado_q<=estado_d;
-   end if;
+   	if reset='1' then
+		estado_q<=Inicio;
+  	 elsif (clk'event and clk='1') then
+		estado_q<=estado_d;
+   	end if;
    end process;
-
---contador                                                                    
+--registro X e Y
+   PROCESS (clk, reset,XCOL)
+   begin
+	if reset='1' then
+		x <= (others => '0');
+	elsif LD_X='1' then
+		x <= XCOL;
+	end if;
+   end process;
+   PROCESS (clk, reset, YROW)
+   begin
+	if reset='1' then
+		y <= (others => '0');
+	elsif LD_Y='1' then
+		y <= YROW;
+	end if;
+   end process;
+--contadorCiclos                                                                   
    PROCESS (clk, reset)
    BEGIN
      IF reset = '1' THEN
        cnt <= (others => '0');
      ELSIF clk'event AND clk='1' THEN
-       IF ENABLE_DATA='1' THEN
+       IF ENABLE_CONT_DATA='1' THEN
          cnt <= cnt + 1;
+       ELSIF LD_CONT_DATA='1' THEN
+	 cnt <= LD_VALUE_CONT_DATA;
        END IF;
      END IF;
    END PROCESS;
- 
-   tc <= '1' WHEN cnt = "111" ELSE '0';
-   count <= std_logic_vector(cnt);
- 
 END arch1;
--- tb_contador.vhd
+++ tb_contador.vhd
@@ -12,9 +12,12 @@ architecture a of tb_cont is
   component counter8
     port ( 
     clk       : IN std_logic; 
-    reset     : IN std_logic;
-    count     : OUT std_logic_vector(3 DOWNTO 0);
-    tc        : OUT std_logic;
+    reset     : IN std_logic; 
+    LCD_init_done : IN std_logic;
+    OP_SETCURSOR : IN std_logic;
+    OP_DRAWCOLOR : IN std_logic;
+    YROW     : IN std_logic_vector(8 downto 0);
+    XCOL      : IN std_logic_vector(7 downto 0);
     LCD_WRN   : OUT std_logic;
     LCD_RS    : OUT std_logic;
     LCD_CSN   : OUT std_logic
@@ -26,12 +29,15 @@ architecture a of tb_cont is
   signal tb_clk       : std_logic := '1'; 
   signal tb_reset     : std_logic := '1'; 
   signal tb_enable    : std_logic := '0';
-  signal tb_count     : std_logic_vector(3 DOWNTO 0);
-  signal tb_tc        : std_logic;
   signal tb_lcd_rs    : std_logic :='0';
   signal tb_lcd_wrn   : std_logic :='0';
   signal tb_lcd_csn   : std_logic :='0';
-  signal tb_enable_data: std_logic :='0';
+  signal tb_op_setcursor : std_logic :='0';
+  signal tb_op_drawcolor : std_logic :='0';
+  signal tb_yrow : std_logic_vector (8 downto 0);
+  signal tb_xcol : std_logic_vector (7 downto 0);
+  signal tb_lcd_init_done : std_logic:='1';
+
 --	...
 --	...
 --	...
@@ -42,11 +48,14 @@ begin
 -- *** incluir todas las señales del port()
     clk      => tb_clk,
     reset    => tb_reset,
-    count    => tb_count,
-    tc       => tb_tc,
     LCD_WRN  => tb_lcd_wrn,
     LCD_CSN  => tb_lcd_csn,
-    LCD_RS   => tb_lcd_rs
+    LCD_RS   => tb_lcd_rs,
+    OP_SETCURSOR => tb_op_setcursor,
+    OP_DRAWCOLOR => tb_op_drawcolor,
+    LCD_init_done => tb_lcd_init_done,
+    YROW => tb_yrow,
+    XCOL => tb_xcol
     );
 
   -- definicion del reloj
