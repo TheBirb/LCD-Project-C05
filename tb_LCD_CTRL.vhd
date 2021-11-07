8c253fbc113d25374bbabe69f91be34139978844
Jon <60443461+TheBirb@users.noreply.github.com>
2021-11-07 19:00:12 +0100

    Update tb_LCD_CTRL.vhd

--- tb_LCD_CTRL.vhd
+++ tb_LCD_CTRL.vhd
@@ -1,6 +1,3 @@
--- Plantilla para creación de testbench
---    xxx debe sustituires por el nombre del módulo a testear
----
 library ieee; 
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -28,36 +25,36 @@ architecture a of tb_cont is
     LCD_DATA : OUT std_logic_vector (15 downto 0)
 	  ); 
   end component ; 
--- *** y declarar como señales internas todas las señales del port()
+-- *** y declarar como seÃ±ales internas todas las seÃ±ales del port()
 -- *** usando el mismo nombre
--- *** Además, pueden inicializarse las entradas para t=0
+-- *** AdemÃ¡s, pueden inicializarse las entradas para t=0
   signal tb_clk       : std_logic := '1'; 
   signal tb_reset     : std_logic := '0'; 
   signal tb_lcd_init_done : std_logic:='1';
-  signal tb_enable    : std_logic := '1';
-  signal tb_lcd_rs    : std_logic :='0';
-  signal tb_lcd_wrn   : std_logic :='0';
-  signal tb_lcd_csn   : std_logic :='0';
+  signal tb_enable    : std_logic := '0';
+  signal tb_lcd_rs    : std_logic :='1';
+  signal tb_lcd_wrn   : std_logic :='1';
+  signal tb_lcd_csn   : std_logic :='1';
   signal tb_op_setcursor : std_logic :='0';
-  signal tb_op_drawcolor : std_logic :='1';
-  signal tb_yrow : std_logic_vector (8 downto 0);
-  signal tb_xcol : std_logic_vector (7 downto 0);
+  signal tb_op_drawcolor : std_logic :='0';
+  signal tb_yrow : std_logic_vector (8 downto 0) := "100010011";
+  signal tb_xcol : std_logic_vector (7 downto 0) :="10001001";
   signal tb_done_cursor: std_logic:='0';
   signal tb_done_colour: std_logic:='0';
   signal tb_lcd_data: std_logic_vector (15 downto 0);
-  signal tb_num_pix : unsigned (16 downto 0) :="00000000000000101";  
-  signal tb_rgb : std_logic_vector (15 downto 0) :="0000000000000000";
+  signal tb_num_pix : unsigned (16 downto 0) := "00000000000000101";  
+  signal tb_rgb : std_logic_vector (15 downto 0) := "1111100000000000";
 
 --	...
 --	...
 --	...
 begin
-  -- instancia del módulo a testear
+  -- instancia del mÃ³dulo a testear
   DUT: LCD_CTRL  
   port map ( 
--- *** incluir todas las señales del port()
+-- *** incluir todas las seÃ±ales del port()
     clk      => tb_clk,
-    reset_l    => tb_reset,
+    reset_l   => tb_reset,
     LCD_WR_N  => tb_lcd_wrn,
     LCD_CS_N  => tb_lcd_csn,
     LCD_RS   => tb_lcd_rs,
@@ -73,13 +70,20 @@ begin
     );
 
   -- definicion del reloj
-  tb_clk <= not tb_clk after 20 ns; -- per?odo 20ns -> 50 MHz
+  tb_clk <= not tb_clk after 10 ns; -- per?odo 20ns -> 50 MHz
 
   -- definicion de estimulos de entrada
   process
     begin
-    wait for 20 ns;      
+    wait for 50 ns;      
       tb_reset <= '1';
+      tb_op_setcursor <= '1';
+    wait for 30 ns;
+	tb_op_setcursor <= '0';
+    wait for 560 ns;
+        tb_op_drawcolor <= '1';
+    wait for 30 ns;
+	tb_op_drawcolor <= '0';
     wait;
     end process;
 end;
